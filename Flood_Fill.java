60a876cac58f308c3d3c05be4b8dc04c3aaf3526
TANISH-PANWAR-RAJPUT <83816536+Tanish-Panwar@users.noreply.github.com>
2021-10-16 10:12:51 +0530

    Added flood fill file

---
+++ Flood_Fill.java
@@ -0,0 +1,45 @@
+public class Flood_Fill {
+    
+    public static void main(String[] args) {
+        int a[][] = {{1, 1, 1, 1, 1, 1, 1, 1},
+                     {1, 1, 1, 1, 1, 1, 0, 0},
+                     {1, 0, 0, 1, 1, 0, 1, 1},
+                     {1, 2, 2, 2, 2, 0, 1, 0},
+                     {1, 1, 1, 2, 2, 2, 2, 0},
+                     {1, 1, 1, 1, 1, 2, 1, 1},
+                     {1, 1, 1, 1, 1, 2, 2, 1}
+                    };
+
+        floodFill(a, 4, 3, 3, 2);
+        printMatrix(a);
+        
+    }
+
+    static void printMatrix(int a[][]){
+        System.out.println("RESULTS");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] +" ");
+            }
+            System.out.println();
+        } 
+    }
+
+    static void floodFill(int a[][], int r, int c, int toFill, int prevFill) {
+        int rows = a.length;
+        int cols = a[0].length;
+
+        if(r<0 || r>=rows || c==0 || c>=cols) {
+            return;
+        }
+        if(a[r][c] != prevFill) {
+            return;
+        }
+
+        a[r][c] = toFill;
+        floodFill(a, r-1, c, toFill, prevFill);
+        floodFill(a, r, c-1, toFill, prevFill);
+        floodFill(a, r+1, c, toFill, prevFill);
+        floodFill(a, r, c+1, toFill, prevFill);
+    }
+}
